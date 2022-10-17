07cd0cdfe627495d97d6d97d86cfe814bd880fbb
aking618 <ashsquads@gmail.com>
2022-10-17 11:33:56 -0500

    AK : Created About Page

---
+++ WebDevProject/Pages/About.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebDevProject.Pages
+
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
