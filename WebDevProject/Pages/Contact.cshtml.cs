824c054a7ee0be450e1d66f2ac138aaf4b6a4938
aking618 <ashsquads@gmail.com>
2022-10-17 14:57:46 -0500

    AK : Created Contact Page

---
+++ WebDevProject/Pages/Contact.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebDevProject.Pages
+{
+    public class ContactModel : PageModel
+    {
+
+        [BindProperty]
+        public string EmailAddress { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            Console.WriteLine(EmailAddress);
+        }
+    }
+}
